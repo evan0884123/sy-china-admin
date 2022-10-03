14f64b9a2b12ebe55d67043e14aca3f4267e6cd1
evan0884123 <87749558+evan0884123@users.noreply.github.com>
2022-10-03 11:23:46 +0400

    init......

---
+++ src/main/java/com/sychina/admin/SyChinaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.sychina.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyChinaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyChinaAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sychina/admin/SyChinaAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sychina.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyChinaAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
